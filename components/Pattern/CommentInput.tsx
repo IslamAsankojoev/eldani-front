fd0d654693e12bb0cdac43d04f8f520e8d205219
Islam Asankojoev <islam.asankojoev@gmail.com>
2024-01-11 18:23:40 +0600

    try add adaptive rows to input

--- components/Pattern/CommentInput.tsx
+++ components/Pattern/CommentInput.tsx
@@ -3,22 +3,26 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/shadcn/ui/avatar'
 import avatar1 from '/public/images/avatar1.jpg'
 import { Button } from '@/shadcn/ui/button'
 import { SendHorizonal } from 'lucide-react'
+import { cn } from '@/lib/utils'
+
+// const rowsHeight = {
+//   '2'
+// }
 
 const CommentInput = () => {
   const ref = useRef<HTMLTextAreaElement>(null)
   const [comment, setComment] = useState('')
+  const [rows, setRows] = useState(1)
+
+  const handleType = (e: any) => {
+    setComment(e.target.value)
+  }
 
   const handleSend = (e: SyntheticEvent<HTMLFormElement, SubmitEvent>) => {
     e.preventDefault()
     setComment('')
     ref.current?.focus()
   }
-
-  // if you want to focus on the input when the modal opens uncomment this
-  // useEffect(() => {
-  //   ref.current?.focus()
-  // }, [])
-
   return (
     <div className="flex items-center p-2 gap-2 bg-stone-100 dark:bg-stone-800">
       <Avatar className="mx-2">
@@ -27,12 +31,14 @@ const CommentInput = () => {
       </Avatar>
       <form className="flex items-center gap-2 justify-between w-full" onSubmit={handleSend}>
         <textarea
-          rows={1}
+          rows={rows}
           ref={ref}
           placeholder="Write a comment..."
           value={comment}
-          onChange={(e) => setComment(e.target.value)}
-          className="flex-1 bg-transparent outline-none focus:outline-none dark:placeholder-stone-400 dark:bg-stone-800 resize-none"
+          onChange={handleType}
+          className={cn(
+            "flex-1 bg-transparent outline-none focus:outline-none dark:placeholder-stone-400 dark:bg-stone-800 resize-none text-nowrap",
+          )}
         />
         <Button variant="ghost" type="submit">
           <SendHorizonal size={25} />
