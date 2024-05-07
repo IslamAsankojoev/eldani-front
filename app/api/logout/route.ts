28416af9cca3d1e2795cdec111b6fda3db13b086
IslamAsankojoev <isla.asankojoev@mail.ru>
2024-05-07 11:30:11 +0600

    fixed session

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -4,7 +4,7 @@ import { type NextRequest } from 'next/server'
 export const dynamic = 'force-dynamic'
 export async function GET(request: NextRequest) {
   const cookie = cookies()
-  cookie.delete('token')
+  cookie.delete('eldani.session')
 
   return Response.redirect(new URL(process.env.URL + '/login', request.url))
 }
